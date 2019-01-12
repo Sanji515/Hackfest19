ebe1bb61e985f2809b6664db5b4e28a5e9dfd55c
Sanjeev Singh <sanjeevsinghdevil97@gmail.com>
2019-01-12 10:04:45 +0530

    modify login

--- Hackfest19/urls.py
+++ Hackfest19/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls', namespace='home'))
+    path('', include('home.urls', namespace='home'))
 ]
