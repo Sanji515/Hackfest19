b064c174c7ea570877975cb0fd21ff6f5df72e9a
Piyush-Karira <piyushkarira1996@gmail.com>
2019-01-12 09:32:34 +0530

    Add login

--- Hackfest19/urls.py
+++ Hackfest19/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls', namespace='home'))
+    path('home/', include('home.urls', namespace='home'))
 ]
---
+++ static/js/forms.js
@@ -0,0 +1,24 @@
+$(window, document, undefined).ready(function() {
+
+  $('.input').blur(function() {
+    var $this = $(this);
+    if ($this.val())
+      $this.addClass('used');
+    else
+      $this.removeClass('used');
+  });
+
+  });
+
+
+$('#tab1').on('click' , function(){
+    $('#tab1').addClass('login-shadow');
+   $('#tab2').removeClass('signup-shadow');
+});
+
+$('#tab2').on('click' , function(){
+    $('#tab2').addClass('signup-shadow');
+   $('#tab1').removeClass('login-shadow');
+
+
+});
